9cbab65a3fa2205ec0de1bde13bc64da48e33378
SoxMax <irishsoxmax@gmail.com>
2021-05-01 10:48:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Path-of-War-Enhancments
+Extension for Fantasy Grounds' Pathfinder ruleset that adds enhancements for Path of War from Dreamscarred Press
---
+++ scripts/pow_data_common.lua
@@ -0,0 +1,5 @@
+table.insert(DataCommon.dmgtypes, "sacred")
+table.insert(DataCommon.dmgtypes, "profane")
+
+table.insert(DataCommon.basicdmgtypes, "sacred")
+table.insert(DataCommon.basicdmgtypes, "profane")

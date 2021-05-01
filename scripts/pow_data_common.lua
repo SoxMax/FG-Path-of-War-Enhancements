76e4e04005014cbc0e6116d048ec7ea018b585dc
SoxMax <irishsoxmax@gmail.com>
2021-05-01 11:12:33 -0400

    use the init function to modify the DataCommon

--- scripts/pow_data_common.lua
+++ scripts/pow_data_common.lua
@@ -1,5 +1,8 @@
-table.insert(DataCommon.dmgtypes, "sacred")
-table.insert(DataCommon.dmgtypes, "profane")
 
-table.insert(DataCommon.basicdmgtypes, "sacred")
-table.insert(DataCommon.basicdmgtypes, "profane")
+function onInit()
+    table.insert(DataCommon.dmgtypes, "sacred")
+    table.insert(DataCommon.dmgtypes, "profane")
+
+    table.insert(DataCommon.basicdmgtypes, "sacred")
+    table.insert(DataCommon.basicdmgtypes, "profane")
+end
